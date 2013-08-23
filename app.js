6c1777bb7f3dcd5e247eca28448cfe314684d58c
ajaswal <aswal.ajay@hotmail.com>
2013-08-23 06:06:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.get('/users', user.list);
 app.get('/', authorTasks.showAll.bind(authorTasks));
 app.post('/addtask', authorTasks.addTask.bind(authorTasks));
 app.get('/view',authorTasks.view.bind(authorTasks));
+app.get('/about',authorTasks.about.bind(authorTasks));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
